2e31e4ee0b47bbd80ea7a2e60809bbdffd057cfc
Martin Jacquet <martin.jacquet@posteo.net>
2019-08-01 16:49:36 +0200

    clean a little bit

--- thegame.py
+++ thegame.py
@@ -1,60 +1,95 @@
 from random import shuffle
 from copy import copy
 
-deck = [i for i in range(2,100)]
+class TheGame:
+	nb_hand = 8
 
-shuffle(deck)
+	def __init__(self):
+		self.mandatory = 2
+		self.deck = [i for i in range(2,100)]
+		shuffle(self.deck)
+		self.hand = []
+		self.draw()
+		self.hand.sort()
+		self.stacks = [100, 100, 1, 1]
+		self.backup()
 
-nb_hand = 8
-hand = []
+	def remain(self): return len(self.deck) > 0
+	def end(self): return len(self.hand) == 0
 
-for i in range(nb_hand):
-	hand += [deck.pop()]
-hand.sort()
+	def backup(self):
+		self.old_stacks = copy(self.stacks)
+		self.old_hand = copy(self.hand)
+		self.old_deck = copy(self.deck)
+	def restore(self):
+		self.deck = self.old_deck
+		self.hand = self.old_hand
+		self.stacks = self.old_stacks
 
-stacks = [100, 100, 1, 1]
+	def draw(self):
+		self.hand.sort()
+		while self.remain() and len(self.hand) < self.nb_hand:
+			self.hand += [self.deck.pop()]
+		if not self.remain():
+			self.mandatory = 1
 
-old_stacks = stacks
-old_hand = hand
-old_deck = deck
+	def display(self):
+		print('----------------------------------')
+		print(' v   v   ^   ^  ')
+		print(self.stacks)
+		print('')
+		print(self.hand)
 
-while len(hand) != 0:
-	if len(deck) > 0: mandatory = 2
-	else: 		  mandatory = 1
-
-	if (nb_hand - len(hand)) >= mandatory and len(deck) > 0:
-		hand.sort()
-		for i in range(nb_hand - len(hand)):
-			hand += [deck.pop()]
-	print ' v   v   ^   ^ '
-	print stacks
-	print ''
-	print hand
-	try:
-		card = input('card: ')
-	except:
-		card = 'merde'
-	if card == '':
-		print 'score :', (len(deck) + len(hand))
+	def score(self):
+		print('----------------------------------')
+		print('Score :', (len(self.deck) + len(self.hand)))
 		exit(0)
-	elif card == 0:
-		deck = old_deck
-		hand = old_hand
-		stacks = old_stacks
-	elif (card not in hand) or (card > 99) or (card < 2):
-		1
-	else:
-		try:
-			stack = input('stack: ')
-		except:
-			continue
-		if stack < 1 or stack > 4:
-			continue
-		old_stacks = copy(stacks)
-		old_hand = copy(hand)
-		old_deck = copy(deck)
-		stacks[stack-1] = card
-		hand.remove(card)
-	print '----------------------------------'
 
-print 'score :', 0,'!!!'
+	def get_card(self):
+		try:
+			read = input('card: ')
+		except:
+			return 'merde'
+		if   read == '':
+			return 'exit'
+		elif read == 0:
+			return 'restore'
+		elif read not in self.hand or read > 99 or read < 2:
+			return 'merde'
+		else:
+			return read
+
+	def get_stack(self):
+		try:
+			read = input('stack: ')
+		except:
+			return 'merde'
+		if not isinstance(read, int) or read < 1 or read > 4:
+			return 'merde'
+		else:
+			return read
+
+	def play(self,card,stack):
+		self.stacks[stack-1] = card
+		self.hand.remove(card)
+
+### MAIN
+game = TheGame()
+
+while not game.end():
+	if (game.nb_hand - len(game.hand)) >= game.mandatory:
+		game.draw()
+	game.display()
+	card = game.get_card()
+	if   card == 'merde':
+		continue
+	elif card == 'exit':
+		break
+	elif card == 'restore':
+		game.restore()
+	else:
+		stack = game.get_stack()
+		if stack != 'merde':
+			game.backup()
+			game.play(card,stack)
+game.score()
