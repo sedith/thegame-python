885518bf7bbf4538818f4edca12df21fbd3ceedf
Martin Jacquet <mjacquet@laas.fr>
2019-08-12 23:44:30 +0200

    add expand to display last 5 values on each stacks ; change backup: save all the stacks and history is list of stacks played in chronological order ; update display ; update todo

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 ## TODO
 
-- More intelligent backup
-- Display 5 last elements on the stacks
 - Check if possible to put on desired stack
 - If only one stack available, autoplay
 - Auto end
+- Change controls/display
--- thegame.py
+++ thegame.py
@@ -1,57 +1,53 @@
 from random import shuffle
 from copy import copy
+from bisect import insort
+from math import log10
 
 class TheGame:
+	# Initialization
 	nb_hand = 8
+	nb_show_stack = 5
 
 	def __init__(self):
-		self.mandatory = 2
 		self.deck = [i for i in range(2,100)]
 		shuffle(self.deck)
 		self.hand = []
 		self.draw()
 		self.hand.sort()
-		self.stacks = [100, 100, 1, 1]
+		self.stacks = [[100], [100], [1], [1]]
 		self.history = []
 
+	# Tests
 	def remain(self): return len(self.deck) > 0
 	def end(self): return len(self.hand) == 0
 
-	def backup(self):
-		self.history.append({'stacks': copy(self.stacks), 'hand': copy(self.hand), 'deck': copy(self.deck)})
-		# self.old_stacks = copy(self.stacks)
-		# self.old_hand = copy(self.hand)
-		# self.old_deck = copy(self.deck)
+	# History
+	def backup(self, stack):
+		self.history.append(stack-1)
 	def restore(self):
-		# self.deck = self.old_deck
-		# self.hand = self.old_hand
-		# self.stacks = self.old_stacks
 		if len(self.history) > 0 :
-			self.deck = self.history[-1]['deck']
-			self.hand = self.history[-1]['hand']
-			self.stacks = self.history[-1]['stacks']
+			if len(self.hand) == self.nb_hand:
+				self.deck.append(self.hand.pop())
+				if self.remain(): self.deck.append(self.hand.pop())
+			insort(self.hand, self.stacks[self.history[-1]].pop())
 			self.history.pop()
 
-	def draw(self):
-		self.hand.sort()
-		while self.remain() and len(self.hand) < self.nb_hand:
-			self.hand.append(self.deck.pop())
-		if not self.remain():
-			self.mandatory = 1
-
+	# Display
 	def controls(self):
 		print('Controls :')
-		print('  ENTER  : exit game')
-		print('  0      : restore previous state')
-		print('  2-99   : play card of inputed value')
-
+		print('  ENTER     exit game')
+		print('  SPACE     display the lasts cards on each stack')
+		print('  0         restore previous state')
+		print('  2-99      play card of inputed value')
+		print('  1-4       play on stack of inputed number')
 	def display(self):
 		print('----------------------------------')
-		print(' v   v   ^   ^  ')
-		print(self.stacks)
+		print('v',int(log10(self.stacks[0][-1]))*' ',' v',int(log10(self.stacks[1][-1]))*' ',' ʌ',int(log10(self.stacks[2][-1]))*' ',' ʌ', sep='')
+		print(self.stacks[0][-1], self.stacks[1][-1], self.stacks[2][-1], self.stacks[3][-1])
 		print('')
-		print(self.hand)
-
+		for i in range(len(self.hand)):
+			print(self.hand[i],end=' ')
+		print('\n')
 	def score(self):
 		score = len(self.deck) + len(self.hand)
 		print('----------------------------------')
@@ -61,11 +57,22 @@ class TheGame:
 		elif score <= 20: print('Not bad ! :)')
 		elif score <= 30: print('Meh :(')
 		exit(0)
+	def show_stacks(self):
+		print()
+		for stack in range(4):
+			nb_show = min(len(self.stacks[stack]),self.nb_show_stack)
+			print(nb_show,'last cards of stack', stack+1, ':\n  ', end='')
+			for i in range(nb_show, 0, -1):
+				print(self.stacks[stack][-i], end = ' ')
+			print()
 
+	# Inputs
 	def get_card(self):
-		read = input('card: ')
-		if 	read == '':
+		read = input('play card : ')
+		if 	 read == '':
 			return 'exit'
+		if   read == ' ':
+			return 'expand'
 		try:
 			read = int(read)
 		except:
@@ -76,27 +83,30 @@ class TheGame:
 			return 'merde'
 		else:
 			return read
-
 	def get_stack(self):
-		read = input('stack: ')
 		try:
-			read = int(read)
+			read = int(input('on stack  : '))
 		except:
-			return 'merde'
+			return 0
 		if read < 1 or read > 4:
-			return 'merde'
+			return 0
 		else:
 			return read
 
+	# Game
+	def draw(self):
+		self.hand.sort()
+		while self.remain() and len(self.hand) < self.nb_hand:
+			self.hand.append(self.deck.pop())
 	def play(self,card,stack):
-		self.stacks[stack-1] = card
+		self.stacks[stack-1].append(card)
 		self.hand.remove(card)
 
 ### MAIN
 game = TheGame()
 game.controls()
 while not game.end():
-	if (game.nb_hand - len(game.hand)) >= game.mandatory:
+	if game.remain and len(game.hand) <= game.nb_hand-2:
 		game.draw()
 	game.display()
 	card = game.get_card()
@@ -106,9 +116,11 @@ while not game.end():
 		break
 	elif card == 'restore':
 		game.restore()
+	elif card == 'expand':
+		game.show_stacks()
 	else:
 		stack = game.get_stack()
-		if stack != 'merde':
-			game.backup()
+		if stack:
+			game.backup(stack)
 			game.play(card,stack)
 game.score()
